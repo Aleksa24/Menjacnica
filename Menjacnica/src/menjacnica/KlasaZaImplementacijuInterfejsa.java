aa30b8284568f655622b97b981ad9a27b0d3c163
Aleksa24 <aleksaivanovic24444@gmail.com>
2017-03-29 20:53:09 +0200

    implementacija interfejsa
    dodat konstruktor u valuti(nije koriscen nigde)
    dodato polje menjacnica klase Menjacnica da bi metode mogle da se
    implementiraju
    implementirane metode

--- Menjacnica/src/menjacnica/KlasaZaImplementacijuInterfejsa.java
+++ Menjacnica/src/menjacnica/KlasaZaImplementacijuInterfejsa.java
@@ -5,22 +5,45 @@ import java.util.GregorianCalendar;
 import menjacnicaInterfejs.MenjacnicaInterfejs;
 
 public class KlasaZaImplementacijuInterfejsa implements MenjacnicaInterfejs {
+	// dodacu objekat klase Menjacnica da bi radio sa metodama
+	public Menjacnica menjacnica;
+	
 
 	@Override
 	public void dodavanjeKursa(Valuta valuta) {
-		// TODO Auto-generated method stub
-
+		Valuta[] niz = menjacnica.getValute();
+		for (int i = 0; i < niz.length; i++) {
+			if(niz[i] != null){
+				niz[i] = valuta;
+				return;
+			}
+		}
+		System.out.println("niz valuta je pun!");
 	}
 
 	@Override
-	public void brisanjeKursa() {
-		// TODO Auto-generated method stub
-
+	public void brisanjeKursa(Valuta valuta) {
+		Valuta[] niz = menjacnica.getValute();
+		for (int i = 0; i < niz.length; i++) {
+			if(niz[i] == valuta){
+				niz[i] = null;
+				return;
+			}
+		}
+		System.out.println("valuta ne postoji!");
 	}
 
 	@Override
 	public Valuta pronadjiValutu(GregorianCalendar datum) {
-		// TODO Auto-generated method stub
+		Valuta[] niz = menjacnica.getValute();
+		for (int i = 0; i < niz.length; i++) {
+			if(niz[i].getKupovniKurs() == datum ||
+					niz[i].getProdajniKurs() == datum ||
+					niz[i].getSrednjiKurs() == datum){
+				return niz[i];
+			}
+		}
+		System.out.println("ne postoji takva valuta u nizu");
 		return null;
 	}
 
